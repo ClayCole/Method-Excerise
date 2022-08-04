57eb99977143e610fc8f5a62e0b52197c7571e49
Clay Coleman <claycoleman94@gamil.com>
2022-08-04 15:05:19 -0400

    began user questions

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MethodExcerise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName}. What is your Favorite Color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine("Interesting, What is your Favorite Animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine($"Last Question {userName}, What is your Favorite Musician?");
+            var musician = Console.ReadLine();
+
+            Console.WriteLine($"I appericate your time {userName}, Have a Good Day!");
+        }
+    }
+}
+
