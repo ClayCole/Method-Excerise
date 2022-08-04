2bbfb7110af37f4254184db927acd047122063e4
Clay Coleman <claycoleman94@gamil.com>
2022-08-04 15:35:12 -0400

    MethodExercise

--- Program.cs
+++ Program.cs
@@ -4,8 +4,28 @@ namespace MethodExcerise
 {
     class Program
     {
+        public static int sum(int num1, int num2)
+        {
+            var answer = num1 + num2;
+            return answer;
+        }
+
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+        }
         static void Main(string[] args)
         {
+
+
+            var amountOfPets = sum(1, 2);
+            var maddness = Multiply(60, 2, 4);
+
+
+            // I feel like I am missing something above as when I run the code it just does the bottom part? 
+
+            // Exercise 1
+
             Console.WriteLine("What is your name?");
             var userName = Console.ReadLine();
 
@@ -26,6 +46,10 @@ namespace MethodExcerise
             Console.WriteLine($"Favorite Color:{color}");
             Console.WriteLine($"Favorite Animal:{animal}");
             Console.WriteLine($"Favorite Musicain:{musician}");
+
+           
+
+
 
         }
     }
